fcb327490ed8da64f93e1720e30620e0c316e344
Raisha Asuma <raisha.asuma@gmail.com>
2022-01-25 11:17:58 -0800

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>January 18th, 2022</div>
+      <div>
+          <h2>Car Insurance</h2>
+          <div>$128.00</div>
+      </div>
+    </>
+  );
+}
+
+export default ExpenseItem;
