9d507c8239f0d9e9fbf315b212516f9941661d4b
Raisha Asuma <raisha.asuma@gmail.com>
2022-01-25 11:49:11 -0800

    added some basic styling to ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+const ExpenseItem = () => {
   return (
-    <>
+    <div className='expense-item'>
       <div>January 18th, 2022</div>
-      <div>
+      <div className='expense-item__description'>
           <h2>Car Insurance</h2>
-          <div>$128.00</div>
+          <div className='expense-item__price'>$128.00</div>
       </div>
-    </>
+    </div>
   );
 }
 
