26d9a2443b73fd306169c6a60875698267350273
Raisha Asuma <raisha.asuma@gmail.com>
2022-01-25 13:10:27 -0800

    added dynamic expenses

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
+const App = () => {
+  const expenses = [
+    {
+      id: "1",
+      title: "toilet paper",
+      amount: 30.12,
+      date: new Date(2022, 1, 12),
+    },
+    {
+      id: "1",
+      title: "paper towels",
+      amount: 30.12,
+      date: new Date(2022, 1, 12),
+    },
+    {
+      id: "1",
+      title: "internet",
+      amount: 93.42,
+      date: new Date(2022, 1, 12),
+    },
+  ];
   return (
     <div className="App">
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem id={expenses[0].id} title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem id={expenses[1].id} title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem id={expenses[2].id} title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+  const year = props.date.getFullYear();
+
   return (
     <div className='expense-item'>
-      <div>January 18th, 2022</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
-          <h2>Car Insurance</h2>
-          <div className='expense-item__price'>$128.00</div>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
