39652f5c50847b1cffcf7e6a0c9be467f65b402a
Raisha Asuma <raisha.asuma@gmail.com>
2022-01-26 14:26:10 -0800

    added a filter-by-year dropdown

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import "./App.css";
+import { react, useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const initialExpenses = [
+  {
+    id: "1",
+    title: "toilet paper",
+    amount: 30.12,
+    date: new Date(2022, 1, 12),
+  },
+  {
+    id: "1",
+    title: "paper towels",
+    amount: 30.12,
+    date: new Date(2022, 1, 12),
+  },
+  {
+    id: "1",
+    title: "internet",
+    amount: 93.42,
+    date: new Date(2022, 1, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "1",
-      title: "toilet paper",
-      amount: 30.12,
-      date: new Date(2022, 1, 12),
-    },
-    {
-      id: "1",
-      title: "paper towels",
-      amount: 30.12,
-      date: new Date(2022, 1, 12),
-    },
-    {
-      id: "1",
-      title: "internet",
-      amount: 93.42,
-      date: new Date(2022, 1, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+  const addExpenseHandler = (newExpense) => {
+    setExpenses(...expenses, newExpense);
+    console.log(expenses);
+  }
+
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses}></Expenses>
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './NewExpense.css';
-import ExpenseForm from './ExpenseForm';
+import React from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
-  return (<div className='new-expense'>
-      <ExpenseForm />
-  </div>);
-}
+const NewExpense = (props) => {
+  // where does 'enteredExpenseData' prop come from?
+  
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    
+    // const expenseData = {
+    //   ...enteredExpenseData,
+    //   id: Math.random().toString()
+    // }
+    // props.onAddExpense(expenseData);
+    console.log(enteredExpenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
 
 export default NewExpense;
