fa886655b39b9f6c6ad6a83fbadd2906964def9e
Raisha Asuma <raisha.asuma@gmail.com>
2022-01-26 12:02:11 -0800

    added a NewExpense and ExpenseForm to add a new expense

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -24,6 +25,7 @@ const App = () => {
   ];
   return (
     <div className="App">
+      <NewExpense />
       <Expenses items={expenses}></Expenses>
     </div>
   );
